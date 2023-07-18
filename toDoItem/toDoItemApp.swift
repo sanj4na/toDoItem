97e27dbffc6cb4563f54a329d6afdbbd28670343
Sanjana Sarathi <sanjaners@gmail.com>
2023-07-18 10:19:49 -0700

    Initial Commit

---
+++ toDoItem/toDoItemApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoItemApp.swift
+//  toDoItem
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct toDoItemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
